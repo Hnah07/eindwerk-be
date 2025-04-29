bb10a4aa26e2ae5a57ad360ea1ec436dc413abc1
Hannah Casier <hannah.casier@me.com>
2025-04-29 14:50:26 +0200

    added concert mvc

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            // $table->foreignId('current_team_id')->nullable();
+            $table->string('profilePicture', 2048)->nullable();
+            $table->string('bio')->nullable();
+            $table->float('longitude')->nullable();
+            $table->float('latitude')->nullable();
+            $table->boolean('isActive')->default(true);
             $table->timestamps();
         });
 
