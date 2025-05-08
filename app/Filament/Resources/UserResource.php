3e113461464586a50ed758c0991640e19b300045
Hannah Casier <hannah.casier@me.com>
2025-05-08 19:25:18 +0200

    modified profile picture

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -34,8 +34,8 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('profilePicture')
-                    ->maxLength(2048)
+                Forms\Components\FileUpload::make('profilePicture')
+                    ->avatar()
                     ->default(null),
                 Forms\Components\TextInput::make('bio')
                     ->maxLength(255)
