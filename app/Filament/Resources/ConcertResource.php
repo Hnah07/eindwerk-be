168ee9636e4ff6dd339dc86214bb818ebbde33e0
Hannah Casier <hannah.casier@me.com>
2025-05-08 14:24:33 +0200

    added concert resource

---
+++ app/Filament/Resources/ConcertResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ConcertResource\Pages;
+use App\Filament\Resources\ConcertResource\RelationManagers;
+use App\Models\Concert;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ConcertResource extends Resource
+{
+    protected static ?string $model = Concert::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required()->maxLength(255),
+                Forms\Components\TextInput::make('date')->required(),
+                Forms\Components\Textarea::make('description')->required()->maxLength(65535)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('id')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('date')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('description'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListConcerts::route('/'),
+            'create' => Pages\CreateConcert::route('/create'),
+            'edit' => Pages\EditConcert::route('/{record}/edit'),
+        ];
+    }
+}
