489ecda818ed2508fc153fbe6bc2ba6fa6417070
Hannah Casier <hannah.casier@me.com>
2025-05-20 15:16:46 +0200

    added artist model and controller

---
+++ app/Http/Controllers/ArtistController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArtistController extends Controller
+{
+    public function index()
+    {
+        $artists = Artist::all();
+        return response()->json($artists);
+    }
+}
