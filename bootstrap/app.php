ad1ca504944de735e4c6aa9bfc021a1489ee603d
Hannah Casier <hannah.casier@me.com>
2025-05-27 11:38:13 +0200

    trying to add header via laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -17,3 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->middlewareGroup('api', [
+    App\Http\Middleware\ForceJsonResponse::class,
+    // Illuminate\Routing\Middleware\SubstituteBindings::class,
+    // Illuminate\Http\Middleware\SetCacheHeaders::class,
+]);
