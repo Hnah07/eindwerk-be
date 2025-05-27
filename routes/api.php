ed5b509fab28b89e3eba80f53842764db7bdf73c
Hannah Casier <hannah.casier@me.com>
2025-05-27 10:05:38 +0200

    added auth to api endpoints

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// create api endpoint for concerts using ConcertController
-// Route::get('/concerts', [App\Http\Controllers\ConcertController::class, 'index']);
-// Route::get('/concerts/{id}', [App\Http\Controllers\ConcertController::class, 'show']);
-// Route::post('/concerts', [App\Http\Controllers\ConcertController::class, 'store']);
-// Route::put('/concerts/{id}', [App\Http\Controllers\ConcertController::class, 'update']);
-// Route::delete('/concerts/{id}', [App\Http\Controllers\ConcertController::class, 'destroy']);
-
-// -> kortere routes
-
-Route::apiResource('concerts', ConcertController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
-//Route::apiResource('users', UserController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
-Route::apiResource('locations', LocationController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
-Route::apiResource('artists', ArtistController::class)->only(['index', 'store', 'show', 'destroy', 'update']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('concerts', ConcertController::class);
+    Route::apiResource('locations', LocationController::class);
+    Route::apiResource('artists', ArtistController::class);
+});
